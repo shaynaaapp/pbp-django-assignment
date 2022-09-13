e165302be0fe129959f2be026bcd30bf8a568d9e
Shayna Putri Fitria <shayna.putri@ui.ac.id>
2022-09-13 15:10:38 +0700

    Mengumpulkan Tugas 2 PBP - Shayna

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_barang_katalog,
+        'nama': 'Shayna Putri Fitria'
+    }
+    return render(request, "katalog.html", context)
